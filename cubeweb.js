e072750bcdb5128d6aab09ff6fa5d41d5c4ce171
aditihaiman <ahaiman00@stuy.edu>
2022-10-17 19:36:01 -0400

    transfered tester to hwebpage

--- cubeweb.js
+++ cubeweb.js
@@ -1,3 +1,31 @@
 console.log("Test");
 
 
+
+let newMelody = generate_melody(5);
+console.log(newMelody);
+
+document.getElementById("melody").innerHTML = newMelody;
+
+document.getElementById("harmony").innerHTML = suggest_harmony(newMelody);
+
+function generate_melody(length){
+    let melody = [];
+    for(let i = 0; i < length; i++){
+        melody.push(Math.floor(Math.random() * 7)+1);
+//        console.log(melody);
+    }
+    return melody;
+}
+
+function suggest_harmony(melody){
+    let harmonies = [[1, 4, 6], [2, 5, 7], [3, 6, 1], [4, 7, 2], [5, 1, 3], [6, 2, 4], [7, 3, 5]];
+    let suggested = [];
+    for(let note of melody){
+        let idx = Math.floor(Math.random()*3);
+//        console.log(idx, note, harmonies[note]);
+        suggested.push(harmonies[note-1][idx]);
+    }
+    return suggested;
+    
+}
--- testing.py
+++ testing.py
@@ -5,6 +5,7 @@ cmajor = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 harmonies = [[1, 4, 6], [2, 5, 7], [3, 6, 1], [4, 7, 2], [5, 1, 3], [6, 2, 4], [7, 3, 5]]
 
 
+
 #input a melody line as int array of scale degrees, e.g. [1, 2, 5, 3, 6, 4, 3, 5, 7, 1]
 def suggestHarmony(notes):
     for note in notes:
