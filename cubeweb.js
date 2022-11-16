36e5c05388e6baa8399ed38aae217c653bc6f91d
aditihaiman <ahaiman00@stuy.edu>
2022-11-16 18:05:30 -0500

    adding documentation

--- cubeweb.js
+++ cubeweb.js
@@ -1,10 +1,18 @@
+//import * as Tone from 'tone'
+
 console.log("Test");
+
+//create a synth and connect it to the main output (your speakers)
+//const synth = new Tone.Synth().toDestination();
+
+//play a middle 'C' for the duration of an 8th note
+//synth.triggerAttackRelease("C4", "8n");
 
 
 function generateNew(){ //onclick of button, generates new melody and several random harmonies
     var x = Number(document.getElementById("length").value);
     let newMelody = generate_melody(x);
-    console.log(newMelody); 
+    console.log(newMelody);
     
     document.getElementById("melody").innerHTML = newMelody;
     
@@ -18,6 +26,13 @@ function generateNew(){ //onclick of button, generates new melody and several ra
     document.getElementById("harmony").innerHTML = harmony;
 }
 
+
+/**
+   * Generates a random melody of a specified length
+   *
+   * @param {number} n The desired length of the melody.
+   * @return {Array} A random melody of length n.
+   */
 function generate_melody(n){ //create random melody line of length n
     let melody = [];
     for(let i = 0; i < n; i++){
@@ -44,8 +59,24 @@ function suggest_harmony(melody){ //suggest harmony given melody
 Given a melody and corresponding harmony (how to deal with rhythm?? how to reduce
     a piece to just basic melody andh harmony??), produce weighted probabilities
     for each corresponding harmony and note of melody.
+
+    SEE AI NOTES
+
+    **see score-transformer**
 */
-function get_weights(melody, harmony){ 
+function update_weights(weights, harmony){ 
+    //traverse melody with associated harmony - compute probability of next harmony given current
+    // let weights = [[0, 0, 0, 0, 0, 0, 0],
+    //                 [0, 0, 0, 0, 0, 0, 0],
+    //                 [0, 0, 0, 0, 0, 0, 0],
+    //                 [0, 0, 0, 0, 0, 0, 0],
+    //                 [0, 0, 0, 0, 0, 0, 0],
+    //                 [0, 0, 0, 0, 0, 0, 0],
+    //                 [0, 0, 0, 0, 0, 0, 0]];
+
+    for(let i = 0; i < harmony.length-1; i++){
+        weights[i][i+1] += float(harmony[i]) / harmony.length;
+    }
 
 
 }
