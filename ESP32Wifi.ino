281ef10e795b012614f47b2a33c4619a133c32c5
aditihaiman <ahaiman00@stuy.edu>
2022-12-11 14:32:21 -0500

    update

--- ESP32Wifi.ino
+++ ESP32Wifi.ino
@@ -1,39 +1,31 @@
 //ESP32 WIFI 
+// this tutorial was super helpful: https://techtutorialsx.com/2018/09/13/esp32-arduino-web-server-receiving-data-from-javascript-websocket-client/
+
 
 #include "WiFi.h"
 #include "SPIFFS.h"
 #include "ESPAsyncWebServer.h"
 
-const char* ssid = "ZHUFONet";
-const char* password = "MilAdi0102";
+const char* ssid = "Barnard Guest";
+const char* password = "";
+
+const int xpin = 13; // x-axis of the accelerometer
+const int ypin = 2; // y-axis
+const int zpin = 32; // z-axis, some pins don't work when using wifi for some reason??
+const int speaker = 25;
+
+const int sampleSize = 10;
+
 
 AsyncWebServer server(80);
-AsyncWebSocket ws("/test");
+AsyncEventSource events("/events");
 
-void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
-    if(type == WS_EVT_CONNECT){
- 
-    Serial.println("Websocket client connection received");
- 
-  } else if(type == WS_EVT_DISCONNECT){
-    Serial.println("Client disconnected");
-    Serial.println("-----------------------");
- 
-  } else if(type == WS_EVT_DATA){
- 
-    Serial.print("Data received: ");
- 
-    for(int i=0; i < len; i++) {
-          Serial.print((char) data[i]);
-    }
- 
-    Serial.println();
-  }
-}
+
 
 void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
+  pinMode(speaker, OUTPUT);
 
   if(!SPIFFS.begin()){
     Serial.println("Error while mounting SPIFFS");
@@ -51,13 +43,22 @@ void setup(){
   Serial.print("Connected to Wifi with IP: ");
   Serial.println(WiFi.localIP());
 
-  ws.onEvent(onWsEvent);
-
-  server.addHandler(&ws);
 
   server.on("/html", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SPIFFS, "/index.html", "text/html");
   });
+
+  server.serveStatic("/", SPIFFS, "/");
+
+  events.onConnect([](AsyncEventSourceClient *client){
+    if(client->lastId()){
+      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+    }
+
+    client->send("hello!", NULL, millis(), 1000);
+  });
+
+  server.addHandler(&events);
 
   server.begin();
 
@@ -67,4 +68,49 @@ void setup(){
 
 
 void loop() {
+  //events.send("data readings in the form of melody", "acc_data", millis());
+
+  //get data from accelerometer and send in the form of scale degree
+  int x = analogRead(xpin); //read from xpin
+  delay(1); //
+  int y = analogRead(ypin); //read from ypin
+  delay(1);
+  int z = analogRead(zpin); //read from zpin
+
+  
+  Serial.print(x); //print raw x value on serial monitor
+  Serial.print("\t");
+  Serial.print(y); //print raw y value on serial monitor
+  Serial.print("\t");
+  Serial.print(z); //print raw z value on serial monitor
+  Serial.print("\t");
+  Serial.println("");
+
+  int note = (int) map(z, 1616, 2410, 1, 7);
+  
+  Serial.print("note: ");
+  Serial.println(note);
+  
+  char toSend[16];
+  itoa(note, toSend, 1);
+
+  Serial.print("send: ");
+  Serial.println(toSend);
+ 
+  events.send(toSend, "acc_data", millis());
+
+  delay(1000);
+  
+}
+
+
+int ReadAxis(int axisPin) {
+  long reading = 0;
+  analogRead(axisPin);
+  delay(1);
+  for (int i = 0; i < sampleSize; i++)
+  {
+  reading += analogRead(axisPin);
+  }
+  return reading/sampleSize;
 }
